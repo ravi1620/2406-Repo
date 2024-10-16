26472b9b2b217747477b289599b892d3f302cc13
Ravi <gravi362000@gmail.com>
2024-10-16 12:03:00 +0530

    no modifications

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Signup() {
   return (
     <div>
-        <form>
-            <div>
-                <input placeholder='First Name'></input>
-                <input placeholder="Middle Name"></input>
-                <input placeholder='Last Name'></input>
-                
-                <input placeholder='Email'></input>
-                <input placeholder="Mobile Number"></input>
-                <textarea placeholder='Address'></textarea>
-            </div>
-        </form>
+      <form>
+        <div>
+          <input placeholder="First Name"></input>
+          <input placeholder="Middle Name"></input>
+          <input placeholder="Last Name"></input>
+          <input placeholder="Email"></input>
+          <input placeholder="Mobile Number"></input>
+          <textarea placeholder="Address"></textarea>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
