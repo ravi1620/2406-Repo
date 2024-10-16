7789873878aa724d205116dd9c1d1743eee9a30b
Ravi <gravi362000@gmail.com>
2024-10-16 11:17:54 +0530

    Signup

---
+++ src/components/Signup.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+        <form>
+            <div>
+                <input placeholder='First Name'></input>
+                <input placeholder='Last Name'></input>
+                <input placeholder='Email'></input>
+                <textarea placeholder='Address'></textarea>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Signup
